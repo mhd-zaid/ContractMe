d102403dfe168d79c6ae0cb84b0fa3d5566bf63a
mhd-zaid <zaidmouhamad@gmail.com>
2021-06-01 17:35:36 +0200

    html page

--- index.js
+++ index.js
@@ -27,10 +27,15 @@ app.use('/contract/:id', (req, response) => {
       var html = fs.readFileSync('htmls/contract.html', 'utf8');
       html = html.replace('{nom}',data.nom)
       html = html.replace('{prenom}',data.prenom)
-      // html = html.replace('{nom}',data.nom)
-      // html = html.replace('{nom}',data.nom)
-      // html = html.replace('{nom}',data.nom)
-      // html = html.replace('{nom}',data.nom)
+      html = html.replace('{DN}',data.date_de_naissance)
+      html = html.replace('{LN}',data.lieu_de_naissance)
+      html = html.replace('{AD}',data.address)
+      html = html.replace('{NS}',data.secu)
+      html = html.replace('{DD}',data.date_de_debut)
+      html = html.replace('{DE}',data.date_edition)
+      html = html.replace('{SB}',data.salaire_brut)
+      html = html.replace('{ST}',data.statut)
+      html = html.replace('{PC}',data.position_coeff)
       pdf.create(html).toStream(function(err, res){
         if (err) return console.log(err);
         response.setHeader('Content-Type', 'application/pdf');
